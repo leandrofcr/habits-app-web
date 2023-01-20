66606b4e516d035036f8d693b23034f2ce970771
Leandro Reis <lleandrofr@gmail.com>
2023-01-20 13:36:11 -0300

    feat: create habit table components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import './styles/global.css';
-
-// import { Habit } from './components/Habit';
-import { Plus } from 'phosphor-react';
 import { Header } from './components/Header';
+import { Plus } from 'phosphor-react';
+import { SummaryTable } from './components/SummaryTable';
+
+import './styles/global.css';
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+      <div className="w-full max-w-5xl px-6 flex font-bold flex-col gap-16">
         <Header />
+        <SummaryTable />
       </div>
     </div>
   );
